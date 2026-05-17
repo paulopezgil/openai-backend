490c5ba37f8c948b628992745578415a8d50df24
Pau López Gil <paulopezgilperez@gmail.com>
2026-05-17 02:08:25 +0200

    django files reorganized

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# openai-backend
+# Local AI Backend
 A OpenAI-compatible Django-based backend that uses llama-cpp-python to run local AI.
--- ai_api/asgi.py
+++ ai_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for app project.
+ASGI config for ai_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_api.settings')
 
 application = get_asgi_application()
